7a7667e57dfde00bdee47b1445f7c38b98213e85
trollmeight <trollm8@proton.me>
2025-03-10 06:11:04 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+const lanyard: React.FC = () => {
+  return (
+    <div className="image">
+     <div className="flex justify-center items-center min-h-screen bg-gray-900 text-white relative">
+      <div className="max-w-2xl p-6 bg-gray-800 rounded-lg shadow-lg relative z-10">
+      <a href="https://discord.com/users/1266754182082461751">
+      <img src="https://lanyard.cnrad.dev/api/1266754182082461751?bg=&theme=dark&showDisplayName=false" />
+      </a>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default lanyard;
