655443b127e572085557dbdc6f8743ca30dee464
trollmeight <trollm8@proton.me>
2025-03-20 07:35:50 -0500

    e

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const lanyard: React.FC = () => {
      <div className="flex justify-center items-center min-h-screen bg-gray-900 text-white relative">
       <div className="max-w-2xl p-6 bg-gray-800 rounded-lg shadow-lg relative z-10">
       <a href="https://discord.com/users/1266754182082461751">
-      <img src="https://lanyard.cnrad.dev/api/1266754182082461751?&theme=dark&showDisplayName=false" />
+      <img src="https://lanyard.cnrad.dev/api/1266754182082461751?bg=222222&theme=dark&showDisplayName=false" />
       </a>
       </div>
       </div>
